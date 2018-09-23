763b98529a429ee4cf4759b690525674a6c1f7af
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2018-09-23 15:19:03 +0530

    Adding only the root-level index file in gitignore and moving the footer code to a seperate footer.php file for the Simple theme

---
+++ simple/index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?> 
+   <main>
+        <?php if(have_posts()): ?>
+            <?php while(have_posts()) : the_post() ?>
+                <div class="container">
+                    <h3><?php the_title();?></h3>
+                    <div class="meta">
+                        Created By: <?php the_author(); ?> on <?php the_time('F j, Y g:i a');?>
+                    </div>
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <div class="container">
+                <?php echo wpautop('Sorry, no posts found.'); ?>
+            </div>
+        <?php endif; ?>
+   </main>
+<?php get_footer(); ?>
