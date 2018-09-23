bb9048ad63560dcfa015f7d0f1481679378b1fc5
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2018-09-23 16:27:56 +0530

    adding a single post view file and creating links from the post-lists to the single post view

--- simple/index.php
+++ simple/index.php
@@ -1,15 +1,23 @@
 <?php get_header(); ?> 
-   <main>
+   <main class="posts-list">
         <?php if(have_posts()): ?>
-            <?php while(have_posts()) : the_post() ?>
-                <div class="container">
-                    <h3><?php the_title();?></h3>
-                    <div class="meta">
-                        Created By: <?php the_author(); ?> on <?php the_time('F j, Y g:i a');?>
-                    </div>
-                    <?php the_content(); ?>
-                </div>
-            <?php endwhile; ?>
+            <div class="container">
+                <?php while(have_posts()) : the_post() ?>
+                    <article>
+                        <h3>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_title();?>
+                            </a>
+                        </h3>
+                        <div class="meta">
+                            Created By: <?php the_author(); ?> on <?php the_time('F j, Y g:i a');?>
+                        </div>
+                        <?php the_content(); ?>
+                        <br/>
+                        <a class="readmore-btn" href="<?php the_permalink();?>"> Read More </a>
+                    </article>
+                <?php endwhile; ?>
+            </div>
         <?php else: ?>
             <div class="container">
                 <?php echo wpautop('Sorry, no posts found.'); ?>
